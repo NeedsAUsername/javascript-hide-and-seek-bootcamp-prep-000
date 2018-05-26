f5f2baecf32aa8d25aba2085e7b5d0f890f31b15
Kevin Chi <kevinchi118@gmail.com>
2018-05-26 15:44:17 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+const rankedclass = document.querySelectorAll('.ranked-list')
+
+function increaseRankBy(n) {
+  rankedclass[0].children[0].innerHTML = parseInt(rankedclass[0].children[0].innerHTML) + n;
+  rankedclass[0].children[1].innerHTML = parseInt(rankedclass[0].children[1].innerHTML) + n;
+  rankedclass[1].children[0].innerHTML = parseInt(rankedclass[1].children[0].innerHTML) + n;
+  rankedclass[1].children[1].innerHTML = parseInt(rankedclass[1].children[1].innerHTML) + n;
+  rankedclass[1].children[2].innerHTML = parseInt(rankedclass[1].children[2].innerHTML) + n;
+}
+
+
+
+/*
+for (var i = 0; i < list.children.length; i += 1) {
+  var x = list.children[i]
+  x.innerHTML = parseInt(x.innerHTML) + n;
+}
+
+*/
