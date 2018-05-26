b09b79531b8cd101d67330c513c38a8a53389505
Kevin Chi <kevinchi118@gmail.com>
2018-05-26 17:12:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function increaseRankBy(n) {
 */
 
 function rankIncrease(child, increase)
-  for (let i = 0); i < rankedclass[x].children.length; i += 1) {
+  for (let i = 0; i < rankedclass[x].children.length; i += 1) {
     rankedclass[x].children[i].innerHTML = parseInt(rankedclass[x].children[i].innerHTML) + n;
   }
 
